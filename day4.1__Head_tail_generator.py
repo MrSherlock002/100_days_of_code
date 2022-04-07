aa67f346babf7584c482b2d2f4e4b6a2271325c6
MrSherlock002 <90151632+MrSherlock002@users.noreply.github.com>
2022-04-07 18:56:53 +0530

    Create day4.1__Head_tail_generator.py

---
+++ day4.1__Head_tail_generator.py
@@ -0,0 +1,26 @@
+###########################################
+## 
+## Prasad Bhosale
+## Thursday (07/04/22)
+## Day 4.1 - Head/tail generator
+##
+###########################################
+
+
+
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+
+import random
+
+a = random.randint(0,1)
+
+if a==0:
+    print("Tails")
+else:
+    print("Heads")
+
+
+
+
+
