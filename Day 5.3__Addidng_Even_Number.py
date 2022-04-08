2a8ccd857a82f41329f400ce17ab6e49bc468891
MrSherlock002 <90151632+MrSherlock002@users.noreply.github.com>
2022-04-08 10:24:19 +0530

    Create Day 5.3__Addidng_Even_Number.py

---
+++ Day 5.3__Addidng_Even_Number.py
@@ -0,0 +1,14 @@
+##########################################################################
+##
+##  Prasad R. Bhosale
+##  Friday (08/04/2022)
+##  Day 5.3__Addidng_Even_Number
+##
+##########################################################################
+
+#Write your code below this row 👇
+
+sum = 0 
+for i in range(0,101,2):    #Adding even number
+    sum+=i
+print(sum)
