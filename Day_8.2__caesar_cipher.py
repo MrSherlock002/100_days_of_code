44a6904e2b5683f844907af6321a3218f98ee294
MrSherlock002 <90151632+MrSherlock002@users.noreply.github.com>
2022-04-11 23:43:02 +0530

    Update Day_8.2__caesar_cipher.py

--- Day_8.2__caesar_cipher.py
+++ Day_8.2__caesar_cipher.py
@@ -61,9 +61,11 @@ def caesar_cipher(alphabet,direction,text,shift):
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 print(logo)
+
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n")
+shift = int(input("Type the shift number:\n"))
+
 caesar_cipher(alphabet,direction,text,shift)
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
